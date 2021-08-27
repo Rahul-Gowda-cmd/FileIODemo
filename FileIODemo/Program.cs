3a9c1abe0551a8d2d73be66a992361bc4e1db817
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-08-27 12:42:44 +0530

    feat:File demo program Added

---
+++ FileIODemo/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+namespace FileIODemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program.FileExists();
+            Program.ReadAllLines();
+            Program.ReadAllText();
+            Program.FileCopy();
+            Program.DeleteFile();
+            ReadthroughStreamReader readstream = new ReadthroughStreamReader();
+            readstream.WriteUsingStreamWriter();
+            readstream.ReadFromStreamReader();
+        }
+        public static void FileExists()
+        {
+            String path = @"C:\Users\HP\csharp\FileIODemo\FileIODemo\Example.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File exists");
+            }
+            Console.ReadKey();
+        }
+
+        public static void ReadAllLines()
+        {
+            String path = @"C:\Users\HP\csharp\FileIODemo\FileIODemo\Example.txt";
+            String[] lines;
+            lines = File.ReadAllLines(path);
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+
+            Console.ReadKey();
+        }
+        
+        public static void ReadAllText()
+        {
+            String path = @"C:\Users\HP\csharp\FileIODemo\FileIODemo\Example.txt";
+            String lines;
+            lines = File.ReadAllText(path);
+            Console.WriteLine(lines);
+
+        }
+        public static void FileCopy()
+        {
+            string path = @"C:\Users\HP\csharp\FileIODemo\FileIODemo\Example.txt";
+            string copypath = @"C:\Users\HP\csharp\FileIODemo\FileIODemo\ExampleNew1.txt";
+
+            File.Copy(path, copypath);
+
+        }
+
+        public static void DeleteFile()
+        {
+            string path = @"C:\Users\HP\csharp\FileIODemo\FileIODemo\ExampleNew1.txt";
+
+            File.Delete(path);
+
+        }
+    }
+}
---
+++ FileIODemo/ReadthroughStreamReader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileIODemo
+{
+    public class ReadthroughStreamReader
+    {
+        public void WriteUsingStreamWriter()
+        {
+            String path = @"C:\Users\HP\csharp\FileIODemo\FileIODemo\Example.txt";
+
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine("Hellow World - .Net is awesome");
+                sr.Close();
+                Console.WriteLine(File.ReadAllText(path));
+            }
+            Console.ReadKey();
+        }
+        public void ReadFromStreamReader()
+        {
+            String path = @"C:\Users\HP\csharp\FileIODemo\FileIODemo\Example.txt";
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                String s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
